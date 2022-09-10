fcaf49de396f1ec0ec27e331c55ac54b82bdd9a0
MARTIN BOSC <matomickfythox@gmail.com>
2022-09-10 08:12:57 +0200

    active synthetic viewbinding

--- app/src/main/java/com/example/bigburger/presentation/burger_list/MainActivity.kt
+++ app/src/main/java/com/example/bigburger/presentation/burger_list/MainActivity.kt
@@ -2,26 +2,16 @@ package com.example.bigburger.presentation.burger_list
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.ProgressBar
-import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.RecyclerView
 import com.example.bigburger.R
 import com.example.bigburger.common.Resource
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var mBurgerList: RecyclerView
-    private lateinit var mErrorMessage: TextView
-    private lateinit var mProgress: ProgressBar
-    private lateinit var mLinError: LinearLayout
-    private lateinit var mBtnRetry: Button
 
     private val burgerListViewModel: BurgerListViewModel by viewModels()
 
@@ -29,15 +19,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //bind ui
-        mBurgerList = findViewById(R.id.burger_list)
-        mErrorMessage = findViewById(R.id.error_message)
-        mProgress = findViewById(R.id.progress_circular)
-        mLinError = findViewById(R.id.lin_error)
-        mBtnRetry = findViewById(R.id.btn_retry)
-
         // set retry btn action
-        mBtnRetry.setOnClickListener {
+        btn_retry.setOnClickListener {
             observeResourceBurger()
         }
 
@@ -50,24 +33,24 @@ class MainActivity : AppCompatActivity() {
             when (resource){
                 is Resource.Loading ->{
                     //display progress bar
-                    mLinError.visibility = View.GONE
-                    mProgress.visibility = View.VISIBLE
-                    mBurgerList.visibility = View.GONE
+                    lin_error.visibility = View.GONE
+                    progress_circular.visibility = View.VISIBLE
+                    burger_list.visibility = View.GONE
                 }
                 is Resource.Error -> {
                     //display error message
-                    mErrorMessage.text = resource.message
-                    mLinError.visibility = View.VISIBLE
-                    mProgress.visibility = View.GONE
-                    mBurgerList.visibility = View.GONE
+                    error_message.text = resource.message
+                    lin_error.visibility = View.VISIBLE
+                    progress_circular.visibility = View.GONE
+                    burger_list.visibility = View.GONE
                 }
                 is Resource.Success -> {
                     //initialize list adapter with data
                     var adapter = BurgersRecyclerViewAdapter(this, resource.data ?: emptyList())
-                    mBurgerList.adapter = adapter
-                    mLinError.visibility = View.GONE
-                    mProgress.visibility = View.GONE
-                    mBurgerList.visibility = View.VISIBLE
+                    burger_list.adapter = adapter
+                    lin_error.visibility = View.GONE
+                    progress_circular.visibility = View.GONE
+                    burger_list.visibility = View.VISIBLE
                 }
             }
         })
